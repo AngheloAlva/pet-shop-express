a1baf974ab4a92ea7543a2b6c4a777bc6a7e0af3
Anghelo Alva <ance.anghelo@gmail.com>
2023-09-01 16:20:37 -0400

    add validate-fields

--- routes/brands.routes.js
+++ routes/brands.routes.js
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { check } from 'express-validator'
 import { createBrand } from '../controllers/brand.controller.js'
+import validateFields from '../middlewares/validate-fields.js'
 
 const router = Router()
 
 router.post('/', [
   check('name', 'The name is required').not().isEmpty(),
-  check('image', 'The image link is required').not().isEmpty()
+  check('image', 'The image link is required').not().isEmpty(),
+  validateFields
 ], createBrand)
 
 export default router
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { check } from 'express-validator'
 import { createUser, deleteUser, getUserById, getUsers } from '../controllers/user.controller.js'
+import validateFields from '../middlewares/validate-fields.js'
 
 const router = Router()
 
@@ -14,11 +15,13 @@ router.post('/', [
   check('name', 'Name is required').not().isEmpty(),
   check('email', 'Email is not valid').isEmail(),
   check('password', 'Password is required').not().isEmpty(),
-  check('password', 'Password must be 6 characters').isLength({ min: 6 })
+  check('password', 'Password must be 6 characters').isLength({ min: 6 }),
+  validateFields
 ], createUser)
 
 router.delete('/:id', [
-  check('id', 'ID is not valid').isMongoId()
+  check('id', 'ID is not valid').isMongoId(),
+  validateFields
 ], deleteUser)
 
 export default router
