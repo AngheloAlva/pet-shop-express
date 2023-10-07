c1f0270107b95db18e33b1a473fab2f8ad68002c
Anghelo Alva <ance.anghelo@gmail.com>
2023-10-07 12:00:26 -0300

    delete create Order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,33 +1,29 @@
 import { request, response } from 'express'
 import Orders from '../models/order.js'
 
-export const createOrder = async (req = request, res = response) => {
-  const { id, userId, RUT, products, total, shippingMethod, shippingAddress } = req.body
-
-  const order = new Orders({
-    id,
-    userId,
-    RUT,
-    products,
-    total,
-    shippingMethod,
-    shippingAddress
-  })
-
-  await order.save()
-
-  res.json({
-    msg: 'Order created',
-    order
-  })
-}
-
 export const getOrders = async (req = request, res = response) => {
   const { limit = 10, from = 0 } = req.query
 
   const [total, orders] = await Promise.all([
     Orders.countDocuments(),
     Orders.find()
+      .skip(Number(from))
+      .limit(Number(limit))
+  ])
+
+  res.json({
+    total,
+    orders
+  })
+}
+
+export const getOrdersByUserId = async (req = request, res = response) => {
+  const { userId } = req.params
+  const { limit = 10, from = 0 } = req.query
+
+  const [total, orders] = await Promise.all([
+    Orders.countDocuments({ userId }),
+    Orders.find({ userId })
       .skip(Number(from))
       .limit(Number(limit))
   ])
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,19 +1,9 @@
-import { check } from 'express-validator'
 import { Router } from 'express'
-import { createOrder, getOrders } from '../controllers/order.controller.js'
-import validateFields from '../middlewares/validate-fields.js'
+import { getOrdersByUserId, getOrders } from '../controllers/order.controller.js'
 
 const router = Router()
 
-router.post('/', [
-  check('userId', 'The userId is required').not().isEmpty(),
-  check('RUT', 'The RUT is required').not().isEmpty(),
-  check('products', 'The products is required').not().isEmpty(),
-  check('total', 'The total is required').not().isEmpty().isNumeric(),
-  check('shippingMethod', 'The shippingMethod is required').not().isEmpty().isIn(['DELIVERY', 'PICKUP']),
-  check('shippingAddress', 'The shippingAddress is required').not().isEmpty(),
-  validateFields
-], createOrder)
+router.get('/:userId', getOrdersByUserId)
 
 router.get('/', getOrders)
 
